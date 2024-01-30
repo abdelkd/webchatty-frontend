1ab8b8801a5064a4cf76410a935c6e3fbafdc419
abdelkd <abdelkarimdjelalda@gmail.com>
2024-01-30 22:52:22 +0100

    fix: new SOCKET_URL env

--- src/socket.ts
+++ src/socket.ts
@@ -4,6 +4,6 @@ const URL = process.env.NODE_ENV === 'production'
   ? process.env.SOCKET_URL as string
   : 'http://localhost:3000';
 
-export const socket = io(URL, {
+export const socket = io("https://webchatty.onrender.com/", {
   autoConnect: false,
 })
