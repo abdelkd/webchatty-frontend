470ffe66e19046521b294e0dfd215a6806a3960a
abdelkd <abdelkarimdjelalda@gmail.com>
2024-01-30 22:45:33 +0100

    fix: new SOCKET_URL env

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client'
 
-const URL = process.env.NODE_ENV === 'production' ? process.env.SOCKET_URL : 'http://localhost:3000';
+const URL = process.env.NODE_ENV === 'production' 
+  ? process.env.SOCKET_URL as string
+  : 'http://localhost:3000';
 
 export const socket = io(URL, {
   autoConnect: false,
